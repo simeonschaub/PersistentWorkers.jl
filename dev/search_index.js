7e9c5e3f54b977361cc0d6cbda359bab6ca04750
Documenter.jl <documenter@juliadocs.github.io>
2023-11-02 11:25:13 +0000

    build based on f566c8e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PersistentWorkers","category":"page"},{"location":"#PersistentWorkers","page":"Home","title":"PersistentWorkers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PersistentWorkers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PersistentWorkers]","category":"page"}]
+}
