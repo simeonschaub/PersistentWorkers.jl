f49bb4fb84f10378786ef08e325794fa30f9ad02
Simeon David Schaub <simeondavidschaub99@gmail.com>
2023-11-22 08:25:00 +0100

    wip

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,14 @@ using Test
 using Random
 using Distributed
 
-include("🏴‍☠️.jl")
+#include("🏴‍☠️.jl")
 
 @testset "PersistentWorkers.jl" begin
     cookie = randstring(16)
     port = 9127 # TODO: make sure port is available?
-    worker = run(`$(Base.julia_cmd()) --project -e "using PersistentWorkers; wait(start_worker_loop($port; cluster_cookie=\"$cookie\"))"`; wait=false)
+    port = rand(9128:9999)
+    #worker = run(`$(Base.julia_cmd()) --project -e "using PersistentWorkers; wait(start_worker_loop($port; cluster_cookie=\"$cookie\"))"`; wait=false)
+    worker = run(`$(Base.julia_exename()) --startup=no --project=. -e "using PersistentWorkers; wait(start_worker_loop($port; cluster_cookie=\"$cookie\"))"`; wait=false)
     @show worker
     try
     cluster_cookie(cookie)
